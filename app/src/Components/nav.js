d50ef20d1fcec3a634047b6c9a6adbc7bea3c385
Abhishek <abhishekch.165@gmail.com>
2022-11-15 19:57:47 +0530

    initial commit

---
+++ app/src/Components/nav.js
@@ -0,0 +1,10 @@
+import "./navStyles.css";
+import { Icon } from "@iconify/react";
+
+export default function nav() {
+  return (
+    <div className="nav">
+      Redux Toolkit <Icon icon="heroicons-solid:shopping-cart" />
+    </div>
+  );
+}
