424438bc6c599cc69852f16370f36a0759350126
Abhishek <abhishekch.165@gmail.com>
2022-11-16 17:27:49 +0530

    final setup

--- app/src/App.js
+++ app/src/App.js
@@ -1,52 +1,54 @@
 import "./App.css";
 import Nav from "./Components/nav";
 import data from "./Components/cartItems";
+import Down from "./Components/downarraow";
+import Up from "./Components/uparrow";
 function App() {
   return (
-    <>
+    <div style={{ textAlign: "center" }}>
       <Nav />
       <div style={{ textAlign: "center", fontSize: "6rem" }}>Your Bag </div>
       {data.map((el) => {
         return (
-          <div key={el.id} style={{ textAlign: "center" }}>
-            <img src={el.img} style={{ height: "5rem", width: "5rem" }} />
-            <h3>{el.title}</h3>
-            <p>{el.price}</p>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-              style={{ height: "1rem", width: "1rem" }}
+          <>
+            <div
+              key={el.id}
+              style={{
+                textAlign: "center",
+                marginLeft: "10px",
+              }}
             >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18"
-              />
-            </svg>
-            <p>{el.amount}</p>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-              style={{ height: "1rem", width: "1rem" }}
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75"
-              />
-            </svg>
-          </div>
+              <div
+                style={{
+                  display: "flex",
+                  margin: "auto",
+                  justifyContent: "center",
+                }}
+              >
+                <p style={{ alignContent: "center", marginRight: "3rem" }}>
+                  <img src={el.img} style={{ height: "5rem", width: "5rem" }} />
+                  <h5>{el.title}</h5>
+                  <p>{el.price}</p>
+                </p>
+                <div style={{ padding: "2rem", cursor: "pointer" }}>
+                  <Up />
+                  <p>{el.amount}</p>
+                  <Down />
+                </div>
+              </div>
+            </div>
+            <div style={{ height: "4rem" }}></div>
+          </>
         );
       })}
-    </>
+      <br />
+      <hr />
+      <h3>TOTAL = $0.00</h3>
+      <br />
+      <button type="button" className="btn btn-outline-danger">
+        CLEAR THE CART
+      </button>
+    </div>
   );
 }
 
